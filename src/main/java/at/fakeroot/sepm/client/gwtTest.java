9587e1b3e8c6b80424f8ca0bdb1a64402a0c5fed
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-04 14:40:50 +0000

    hab kurz mal ein GWT mit GoogleMaps Test gemacht. Um zu schaun obs eh ueberall geht
    git-svn-id: https://svn.r7r.at/sepm/trunk@60 240b89b8-79f3-4156-a269-5c34a98ffd57

--- src/main/java/at/fakeroot/sepm/client/gwtTest.java
+++ src/main/java/at/fakeroot/sepm/client/gwtTest.java
@@ -3,6 +3,7 @@ package at.fakeroot.sepm.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -10,9 +11,24 @@ import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment.HorizontalAlignmentConstant;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
+
+import com.google.gwt.maps.client.InfoWindow;
+import com.google.gwt.maps.client.InfoWindowContent;
+import com.google.gwt.maps.client.MapType;
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.control.LargeMapControl;
+import com.google.gwt.maps.client.control.MenuMapTypeControl;
+import com.google.gwt.maps.client.control.OverviewMapControl;
+import com.google.gwt.maps.client.event.MapClickHandler;
+import com.google.gwt.maps.client.event.MarkerClickHandler;
+import com.google.gwt.maps.client.geom.LatLng;
+import com.google.gwt.maps.client.overlay.Marker;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -61,5 +77,63 @@ public class gwtTest implements EntryPoint {
         dialogBox.show();
       }
     });
+    
+    
+    
+    //Map machen
+    HorizontalPanel mapHoPa = new HorizontalPanel();
+    final VerticalPanel markerVePa = new VerticalPanel();
+    markerVePa.add(new HTML("Bitte auf map Clicken"));
+    mapHoPa.setSize("100%", "500px");
+    mapHoPa.setBorderWidth(1);
+    RootPanel.get("mapDiv").add(mapHoPa);
+    
+    //Map Machen
+    final MapWidget myMap = new MapWidget();
+    myMap.setSize("500px", "500px");
+    mapHoPa.add(myMap);
+    myMap.addMapType(MapType.getEarthMap());
+    myMap.addMapType(MapType.getPhysicalMap());
+    myMap.addControl(new LargeMapControl());
+    myMap.addControl(new MenuMapTypeControl());
+    myMap.addControl(new OverviewMapControl());
+    myMap.setCenter(LatLng.newInstance(48.19538740833338, 16.34765625), 13);
+    
+    //Seite machen
+    mapHoPa.add(markerVePa);
+    markerVePa.setWidth("100%");
+    myMap.addMapClickHandler(new MapClickHandler(){
+		public void onClick(MapClickEvent event) {
+			final Marker tempMarker = new Marker(event.getLatLng());
+			myMap.addOverlay(tempMarker);		
+			
+			HTML tempMyNews = new HTML(markerVePa.getWidgetCount()
+					+".) Lat: "+event.getLatLng().getLatitude()
+					+" Lng: "+event.getLatLng().getLongitude());
+			
+			markerVePa.add(tempMyNews);
+			
+			
+			final InfoWindowContent myTempCont = new InfoWindowContent(
+					new HTML("You Clicked: "+markerVePa.getWidgetCount()
+							+".) <br />Lat: "+event.getLatLng().getLatitude()
+							+" <br />Lng: "+event.getLatLng().getLongitude()));
+			
+			tempMyNews.addClickHandler(new ClickHandler(){
+				public void onClick(ClickEvent arg0) {
+					myMap.getInfoWindow().open(tempMarker.getLatLng(), myTempCont);		
+				}
+				
+			});
+			
+			tempMarker.addMarkerClickHandler(new MarkerClickHandler(){
+				public void onClick(MarkerClickEvent event) {
+					myMap.getInfoWindow().open(event.getSender().getLatLng(), myTempCont);
+				}
+			});
+		}
+    	
+    });
+    
  }
 }
