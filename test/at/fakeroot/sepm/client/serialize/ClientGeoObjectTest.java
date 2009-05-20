3c23956c574689a9258949688455e41313b785be
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-20 17:30:45 +0000

    JUnit test fuer ClientGeoObj erstellt
    git-svn-id: https://svn.r7r.at/sepm/trunk@131 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ test/at/fakeroot/sepm/client/serialize/ClientGeoObjectTest.java
@@ -0,0 +1,41 @@
+package at.fakeroot.sepm.client.serialize;
+
+import junit.framework.TestCase;
+
+public class ClientGeoObjectTest extends TestCase {
+
+	
+	public String getModulName(){
+		return "at.fakeroot.sepm.client.serialize.ClientGeoObject";
+	}
+	
+	public void testSimple(){
+		int id=10;
+		String titel="testTitel";
+		String url="bild.png";
+		String[] tags={"tag1","tag2"};
+		double xPos=1.1;
+		double yPos=2.2;
+		
+
+		ClientGeoObject testObject=new ClientGeoObject(id,titel,url,tags,xPos,yPos);
+		
+		assertNotNull(testObject);
+		assertEquals(id, testObject.getId());
+		assertEquals(titel, testObject.getTitel());
+		assertEquals(url, testObject.getImageUrl());
+		assertEquals(tags[0], testObject.getTags()[0]);
+		assertEquals(tags[1], testObject.getTags()[1]);
+		assertEquals(xPos, testObject.getXPos());
+		assertEquals(yPos, testObject.getYPos());
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+}
