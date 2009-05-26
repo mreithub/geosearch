1b1d9df8b810a49a58ec23a03e5395a2647968ab
anca_cismasiu <anca_cismasiu@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-26 00:31:46 +0000

    property klasse fertig
    git-svn-id: https://svn.r7r.at/sepm/trunk@164 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ src/main/java/at/fakeroot/sepm/server/Property.java
@@ -0,0 +1,41 @@
+package at.fakeroot.sepm.server;
+
+/**
+ * @author Anca Cismasiu
+ * Class representing object properties
+ * */
+
+public class Property {
+
+	private String name;
+	private String value;
+
+	/**
+	 * Constructor
+	 * @param name String property name
+	 * @param value String property value
+	 * */	
+	public Property(String name, String value){
+		this.name=name;
+		this.value=value;
+	}
+
+	/**
+	 * Getter for property name
+	 * @return String property name
+	 * */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Getter for property value
+	 * @return String property value
+	 * */
+	public String getValue() {
+		return value;
+	}
+
+
+
+}
