33f68b612c4d5f84ad1c7561d4a96cf42f7f3974
anca_cismasiu <anca_cismasiu@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-26 00:43:25 +0000

    DBGeoObject fertig,noch nicht kommentiert- nur getter
    git-svn-id: https://svn.r7r.at/sepm/trunk@165 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ src/main/java/at/fakeroot/sepm/server/DBGeoObject.java
@@ -0,0 +1,54 @@
+package at.fakeroot.sepm.server;
+
+import at.fakeroot.sepm.client.serialize.GeoObject;
+
+/**
+ * @author Anca Cismasiu
+ *
+ */
+public class DBGeoObject extends GeoObject {
+
+	private int svc_id;
+	private int uid;
+	private String link;
+	private String valid_until;
+	private Property[] properties;
+	private String[] tags;
+	
+	public DBGeoObject(int serviceID, int uniqueID, String link, String valid_until, Property[] properties, String[] tags){
+		svc_id= serviceID;
+		uid=uniqueID;
+		this.link=link;
+		this.valid_until=valid_until;
+		this.properties=properties;
+		this.tags=tags;
+	}
+
+	public int getSvc_id() {
+		return svc_id;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public String getValid_until() {
+		return valid_until;
+	}
+
+	public Property[] getProperties() {
+		return properties;
+	}
+
+	public String[] getTags() {
+		return tags;
+	}
+	
+	
+	
+	
+}
