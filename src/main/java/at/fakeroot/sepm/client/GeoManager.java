b56b20a63ac49bdb3bf8929c3e5b3f0537f9a1b7
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-26 18:10:33 +0000

    Logo zur GUI hinzugefuehgt
    git-svn-id: https://svn.r7r.at/sepm/trunk@169 240b89b8-79f3-4156-a269-5c34a98ffd57

--- src/main/java/at/fakeroot/sepm/client/GeoManager.java
+++ src/main/java/at/fakeroot/sepm/client/GeoManager.java
@@ -4,6 +4,7 @@ package at.fakeroot.sepm.client;
 import java.util.ArrayList;
 
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.PopupPanel;
 import at.fakeroot.sepm.client.serialize.BoundingBox;
 import at.fakeroot.sepm.client.serialize.ClientGeoObject;
@@ -14,6 +15,9 @@ import at.fakeroot.sepm.client.serialize.ClientGeoObject;
  *
  */
 public class GeoManager implements IGeoManager {
+	private int XOFFSET=5;
+	private int YOFFSET=5;
+	private Image logo = new Image("images/design/logo_no_shaddow.png");
 	private SearchBox searchBox;
 	private ResultInfoBox resultBox;
 	private TagCloud tagCloud;
@@ -37,20 +41,32 @@ public class GeoManager implements IGeoManager {
 	}
 	
 	public void drawGUI(){
+		//Logo
+		PopupPanel logoPop = new PopupPanel(false);
+		logo.setPixelSize(190, 40);
+		logoPop.setWidget(this.logo);
+		logoPop.show();
+		logoPop.setPopupPosition(XOFFSET, YOFFSET);
+		
+		//SearchBox
 		PopupPanel searchPop = new PopupPanel(false);
 		searchPop.setWidget(this.searchBox);
-		searchPop.show();
-		searchPop.setPopupPosition(5, 5);
+		searchPop.setPopupPosition(XOFFSET, YOFFSET+60);
+		searchPop.show();		
 		
-		PopupPanel tagPop = new PopupPanel(false);
-		tagPop.setWidget(tagCloud);
-		tagPop.setPopupPosition(5, 180);
-		tagPop.show();
-		
+		//ResultBox
 		PopupPanel infoPop = new PopupPanel(false);
 		infoPop.setWidget(resultBox);
-		infoPop.setPopupPosition(5, 80);
+		infoPop.setPopupPosition(XOFFSET, YOFFSET+130);
 		infoPop.show();
+		
+		//TagCloude
+		PopupPanel tagPop = new PopupPanel(false);
+		tagPop.setWidget(tagCloud);
+		tagPop.setPopupPosition(5, 230);
+		tagPop.show();
+		
+		
 	}
 	
 	public void addSearchTag(String tag) {
@@ -84,6 +100,8 @@ public class GeoManager implements IGeoManager {
 		
 		tagCloud.refresh(testList.iterator());
 		
+		resultBox.refresh(randTag, randTag+10);
+		
 	}
 
 	public void setBoundingBox(BoundingBox box) {
@@ -98,9 +116,11 @@ public class GeoManager implements IGeoManager {
 			final DetailView waitingDeVi = geoMap.createDetailView(geoObject);
 		///TODO benotigt ObjectDetail		
 			//detailView.setDetail(....);
+			final String wasSollISagen="testWaitsuper";
+			
 			Timer t = new Timer(){
 				public void run() {
-					waitingDeVi.setDetail("testWait: "+Math.random());					
+					waitingDeVi.setDetail(wasSollISagen+": "+Math.random());					
 				}				
 			};
 			t.schedule(1000);
