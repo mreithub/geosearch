3ce018743baebc1af2be2faa04de504708887f43
ruth_kendlbacher <ruth_kendlbacher@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-27 19:11:44 +0000

    SearchResultTest fertig
    git-svn-id: https://svn.r7r.at/sepm/trunk@192 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ src/test/java/at/fakeroot/sepm/client/serialize/SearchResultTest.java
@@ -0,0 +1,29 @@
+package at.fakeroot.sepm.client.serialize;
+import junit.framework.TestCase;
+/**
+ * SearchResult JUnitTest
+ * @author RK
+ */
+
+public class SearchResultTest extends TestCase
+{
+	public String getModulName()
+	{
+		return ("at.fakeroot.sepm.client.serialize.SearchResult");
+	}
+	
+	public void testSimple(){
+		ClientGeoObject cgo = new ClientGeoObject(2,"bla", null, new String[]{"eins", "zwei", "drei"}, 14.987, 15.564);
+		int hits = 7;
+		SearchResult testObject = new SearchResult();
+		testObject.addResultToList(cgo);
+		testObject.setResultCount(hits);
+		
+		assertNotNull(testObject);
+		assertEquals(hits, testObject.getResultCount());
+		assertEquals(testObject.getResults().get(0).getId(), cgo.getId());
+		assertEquals(testObject.getResults().get(0).getXPos(), cgo.getXPos());
+		assertEquals(testObject.getResults().get(0).getTitel(), cgo.getTitel());
+		assertEquals(testObject.getResults().get(0).getTags()[0], cgo.getTags()[0]);
+	}
+}
