3dcb6b3270509d75c95d523da39ebf6ea5dc8194
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-27 17:26:07 +0000

    Property JUnit test gemacht
    git-svn-id: https://svn.r7r.at/sepm/trunk@186 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ src/test/java/src/at/fakeroot/sepm/server/PropertyTest.java
@@ -0,0 +1,27 @@
+package src.at.fakeroot.sepm.server;
+
+import at.fakeroot.sepm.server.Property;
+import junit.framework.TestCase;
+
+/**
+ * Property JUnit Test
+ * @author JB
+ *
+ */
+public class PropertyTest extends TestCase {
+
+	public String getModulName(){
+		return "at.fakeroot.sepm.server.Property";
+	}
+	
+	public void testSimple(){
+		String name="protTest";
+		String value="valueTest";
+		
+		Property testObject = new Property(name,value);
+		
+		assertNotNull(testObject);
+		assertEquals(name, testObject.getName());
+		assertEquals(value, testObject.getValue());
+	}
+}
