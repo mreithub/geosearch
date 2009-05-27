2fa6fa16bf04931de52a8df73375df0e913c09f5
ruth_kendlbacher <ruth_kendlbacher@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-27 18:24:41 +0000

    BoundingBoxTest passt
    git-svn-id: https://svn.r7r.at/sepm/trunk@190 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ src/test/java/at/fakeroot/sepm/client/serialize/BoundingBoxTest.java
@@ -0,0 +1,29 @@
+package at.fakeroot.sepm.client.serialize;
+import junit.framework.TestCase;
+/**
+ * BoundingBox JUnitTest
+ * @author RK
+ */
+public class BoundingBoxTest extends TestCase
+{
+
+	public String getModulName()
+	{
+		return ("at.fakeroot.sepm.client.serialize.BoundingBox");
+	}
+	
+	public void testSimple(){
+		double x1 = 14.897;
+		double x2 = 17.986;
+		double y1 = 45.789;
+		double y2 = 46.67;
+		
+		BoundingBox testObject = new BoundingBox(x1,y1,x2,y2);
+		
+		assertNotNull(testObject);
+		assertEquals(x1, testObject.getX1());
+		assertEquals(x2, testObject.getX2());
+		assertEquals(y1, testObject.getY1());
+		assertEquals(y2, testObject.getY2());
+	}
+}
