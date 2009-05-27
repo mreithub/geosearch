df6a5b1b5afc07558adaf5c444447738a68a7a2c
ruth_kendlbacher <ruth_kendlbacher@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-05-27 15:22:04 +0000

    weitere tags hinzugefügt
    git-svn-id: https://svn.r7r.at/sepm/trunk@183 240b89b8-79f3-4156-a269-5c34a98ffd57

--- src/main/java/at/fakeroot/sepm/server/ObjectSearchServiceImpl.java
+++ src/main/java/at/fakeroot/sepm/server/ObjectSearchServiceImpl.java
@@ -21,17 +21,17 @@ public class ObjectSearchServiceImpl extends RemoteServiceServlet implements Obj
 	{
 		//geoObjManager = GeoObjectManager.getInstance(); (nach MR2)
 		fakeDB = new ArrayList<ClientGeoObject>();
-		fakeDB.add(new ClientGeoObject(1, "Karlskirche", null, new String[] {"karlskirche","kirche", "karlsplatz"}, 48.198247, 16.371422));
-		fakeDB.add(new ClientGeoObject(2, "Stephansdom", null, new String[] {"stephansdom","kirche", "stephansplatz", "wahrzeichen", "wien"}, 48.208333, 16.372778));
-		fakeDB.add(new ClientGeoObject(3, "Schloss SchÃ¶nbrunn", null, new String[] {"schloss","schÃ¶nbrunn", "schlosspark", "neptunbrunnen"}, 48.184517, 16.311864));				
-		fakeDB.add(new ClientGeoObject(4, "Goldenes Dachl", null, new String[] {"goldenes","dachl", "innsbruck", "wahrzeichen"}, 47.268583, 11.393264));
-		fakeDB.add(new ClientGeoObject(5, "Festung Hohensalzburg", null, new String[] {"festung","hohensalzburg", "salzburg", "wahrzeichen", "burg"}, 47.794967, 13.047256));
-		fakeDB.add(new ClientGeoObject(6, "Wallfahrtsbasilika PÃ¶stlingberg", null, new String[] {"wallfahrtsbasilika","pÃ¶stlingberg", "linz", "wahrzeichen"}, 48.323889, 14.258333));
-		fakeDB.add(new ClientGeoObject(7, "Uhrturm", null, new String[] {"uhrturm", "turm", "graz", "wahrzeichen"}, 47.075463, 15.436746));
-		fakeDB.add(new ClientGeoObject(8, "Martinsturm", null, new String[] {"martinsturm", "turm", "bregenz", "wahrzeichen"}, 47.505, 9.749167));
-		fakeDB.add(new ClientGeoObject(9, "Schloss Esterhazy ", null, new String[] {"schloss","esterhazy ", "eisenstadt", "wahrzeichen"}, 47.848611, 16.520833));
-		fakeDB.add(new ClientGeoObject(10, "Lindwurm", null, new String[] {"lindwurm","klagenfurt", "wahrzeichen"}, 46.623997, 14.3077));
-		fakeDB.add(new ClientGeoObject(11, "Rathaus", null, new String[] {"rathaus","sankt", "st.", "pÃ¶lten", "wahrzeichen"}, 48.2, 15.616667));
+		fakeDB.add(new ClientGeoObject(1, "Karlskirche", null, new String[] {"karlskirche","kirche", "karlsplatz", "sehenswürdigkeit"}, 48.198247, 16.371422));
+		fakeDB.add(new ClientGeoObject(2, "Stephansdom", null, new String[] {"stephansdom","kirche", "stephansplatz", "wahrzeichen", "wien", "sehenswürdigkeit", "mittelalter"}, 48.208333, 16.372778));
+		fakeDB.add(new ClientGeoObject(3, "Schloss SchÃ¶nbrunn", null, new String[] {"schloss","schÃ¶nbrunn", "schlosspark", "neptunbrunnen", "sehenswürdigkeit"}, 48.184517, 16.311864));				
+		fakeDB.add(new ClientGeoObject(4, "Goldenes Dachl", null, new String[] {"goldenes","dachl", "innsbruck", "wahrzeichen", "sehenswürdigkeit", "mittelalter"}, 47.268583, 11.393264));
+		fakeDB.add(new ClientGeoObject(5, "Festung Hohensalzburg", null, new String[] {"festung","hohensalzburg", "salzburg", "wahrzeichen", "burg", "sehenswürdigkeit", "mittelalter"}, 47.794967, 13.047256));
+		fakeDB.add(new ClientGeoObject(6, "Wallfahrtsbasilika PÃ¶stlingberg", null, new String[] {"wallfahrtsbasilika","pÃ¶stlingberg", "linz", "wahrzeichen", "kirche", "sehenswürdigkeit"}, 48.323889, 14.258333));
+		fakeDB.add(new ClientGeoObject(7, "Uhrturm", null, new String[] {"uhrturm", "turm", "graz", "wahrzeichen", "sehenswürdigkeit", "mittelalter"}, 47.075463, 15.436746));
+		fakeDB.add(new ClientGeoObject(8, "Martinsturm", null, new String[] {"martinsturm", "turm", "bregenz", "wahrzeichen", "sehenswürdigkeit"}, 47.505, 9.749167));
+		fakeDB.add(new ClientGeoObject(9, "Schloss Esterhazy ", null, new String[] {"schloss","esterhazy ", "eisenstadt", "wahrzeichen", "sehenswürdigkeit", "mittelalter"}, 47.848611, 16.520833));
+		fakeDB.add(new ClientGeoObject(10, "Lindwurm", null, new String[] {"lindwurm","klagenfurt", "wahrzeichen", "sehenswürdigkeit"}, 46.623997, 14.3077));
+		fakeDB.add(new ClientGeoObject(11, "Rathaus", null, new String[] {"rathaus","sankt", "st.", "pÃ¶lten", "wahrzeichen", "sehenswürdigkeit", "mittelalter"}, 48.2, 15.616667));
 	}
 	
 	public SearchResult search(BoundingBox box, String what)
