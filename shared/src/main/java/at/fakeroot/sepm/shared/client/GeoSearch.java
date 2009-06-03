77534874fa8483b8aa4568ec5ba90f52d135acae
manuel_reithuber <manuel_reithuber@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-03 20:57:53 +0000

    fehlende dateien hinzugefügt
    git-svn-id: https://svn.r7r.at/sepm/trunk@239 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ shared/src/main/java/at/fakeroot/sepm/shared/client/GeoSearch.java
@@ -0,0 +1,19 @@
+package at.fakeroot.sepm.shared.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GeoSearch implements EntryPoint
+{
+	/**
+	* This is the entry point method.
+	*/
+	public void onModuleLoad()
+	{
+		// do nothing
+	}
+}
