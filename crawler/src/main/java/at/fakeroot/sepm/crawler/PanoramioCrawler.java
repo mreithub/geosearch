de165aa045b6b4697bf01b0660ab404ed8f893c4
anca_cismasiu <anca_cismasiu@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-10 22:09:03 +0000

    panoramio crawler
    git-svn-id: https://svn.r7r.at/sepm/trunk@291 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ crawler/src/main/java/at/fakeroot/sepm/crawler/PanoramioCrawler.java
@@ -0,0 +1,82 @@
+package at.fakeroot.sepm.crawler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import at.fakeroot.sepm.shared.client.serialize.BoundingBox;
+import at.fakeroot.sepm.shared.server.DBGeoObject;
+import at.fakeroot.sepm.shared.server.Property;
+
+public class PanoramioCrawler extends ACrawler{
+
+	public PanoramioCrawler() throws IOException{
+		super("panoramio.com");
+	}
+		
+
+	@Override
+	void crawlBox(BoundingBox curBox) {
+		ArrayList<DBGeoObject> foundObjects = new ArrayList<DBGeoObject>();
+		int from=0, to=0, count=0;
+		String url="";
+		String responseStr = "";
+		
+		while(to<=count){
+			url = "http://www.panoramio.com/map/get_panoramas.php?order=popularity&set=public" +
+					"&from=" + from + "&to=" + to +
+					"&minx=" + curBox.getX1()+ "&miny=" + curBox.getY1()+ 
+					"&maxx=" + curBox.getX2()+ "&maxy=" + curBox.getY2()+ "&size=small";
+		
+			responseStr = requestUrl(url);
+		
+			try {
+				JSONObject jsonResponse = new JSONObject(responseStr);
+				JSONArray photoArray = jsonResponse.getJSONArray("photos");
+				
+				for(int i=0; i<=photoArray.length(); i++){
+					
+					JSONObject jsonObj = new JSONObject(photoArray.getJSONObject(i));
+					
+					Property[] tmpProp = new Property[2];
+					tmpProp[0]= new Property("owner", jsonObj.getString("owner_name"));
+					tmpProp[1]= new Property("photo_url", jsonObj.getString("photo_url"));
+					
+					ArrayList<String> tags=new ArrayList<String>();
+					parseStringIntoTags( jsonObj.getString("photo_title"), tags, true);
+					
+					DBGeoObject tmpObj = new DBGeoObject(jsonObj.getString("photo_title"), 
+														jsonObj.getDouble("longitude"),
+														jsonObj.getDouble("latitude"), 
+														getSvcID(), 
+														jsonObj.numberToString(jsonObj.getInt("photo_id")),
+														jsonObj.getString("photo_url"),
+														null, 
+														tmpProp,
+														(String [])tags.toArray() );
+					foundObjects.add(tmpObj);
+				}
+				
+				saveObject((DBGeoObject[])foundObjects.toArray());
+			
+				count=jsonResponse.getInt("count");
+				from=to;
+				if(count<to+99)
+					to+=99;
+				else to=count;
+			
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}	
+		}	
+	}
+
+	
+	public static void main(String args[]) throws IOException{
+		new PanoramioCrawler();
+	}
+
+}
