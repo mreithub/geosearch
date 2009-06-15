e44e09d45c89a66d19a330f7c27b46c8b2f92023
ruth_kendlbacher <ruth_kendlbacher@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-15 16:56:55 +0000

    WikiCrawler kommentiert
    git-svn-id: https://svn.r7r.at/sepm/trunk@336 240b89b8-79f3-4156-a269-5c34a98ffd57

--- crawler.wikipedia/src/main/java/at/fakeroot/sepm/crawler/wikipedia/WikiCrawler.java
+++ crawler.wikipedia/src/main/java/at/fakeroot/sepm/crawler/wikipedia/WikiCrawler.java
@@ -13,20 +13,43 @@ import at.fakeroot.sepm.shared.client.serialize.BoundingBox;
 import at.fakeroot.sepm.shared.server.DBGeoObject;
 import at.fakeroot.sepm.shared.server.Property;
 
+/**
+ * WikiCrawler searches for geotagged Wikipedia articles, to do so, it uses
+ * the Wikipedia Geocoding Webservice http://www.geonames.org/export/wikipedia-webservice.html.
+ * The summarys of the found articles (together with link, title, geoposition,...) are saved 
+ * in the DB.  
+ * @author RK
+ */
 public class WikiCrawler extends ACrawler
 {
+	//limit for the number of articles, for one request
 	private static final int limit = 200;
 	
+	/**
+	 * Constructor
+	 * @throws IOException
+	 */
 	public WikiCrawler() throws IOException
 	{
 		super("wikipedia.de");
 	}
 	
-	public static void main(String main[]) throws IOException, NotBoundException {
+	/**
+	 * main-method to start crawling
+	 * @param main
+	 * @throws IOException
+	 * @throws NotBoundException
+	 */
+	public static void main(String main[]) throws IOException, NotBoundException 
+	{
 		ACrawler crawler = new WikiCrawler();
 		crawler.crawl();
 	}
 
+	/**
+	 * the method scanns a defined area for Wikipedia articles 
+	 * @param curBox the BoundingBox (area), which is scanned
+	 */
 	protected void crawlBox(BoundingBox curBox) 
 	{
 		ArrayList<DBGeoObject> dbGeoArray = new ArrayList<DBGeoObject>();
@@ -42,6 +65,10 @@ public class WikiCrawler extends ACrawler
 			JSONArray jsonResultArray  = jsonResult.getJSONArray("geonames");
 			if(jsonResultArray.length() == 0)
 				return;
+			//the service doesn't give any information on how many articles altogether are in the area,
+			//beside the ones, which are returned. so, if there are as many rows returned, as the maximum
+			//is, then the BoundingBox is divided in 4 BoundingBoxes, and for each of them, there will 
+			//be a new request.
 			if(jsonResultArray.length() >= limit)
 			{
 				crawlBox(new BoundingBox(curBox.getX1(), (curBox.getY1()+ curBox.getY2())/2, (curBox.getX1()+ curBox.getX2())/2, curBox.getY2()));
@@ -65,7 +92,8 @@ public class WikiCrawler extends ACrawler
 					String[] dbObjTags = tags.toArray(new String[tags.size()]);
 					dbGeoArray.add(new DBGeoObject(title, xPos, yPos, getSvcID(), title, link, null, property, dbObjTags));
 				}
-				//saveObject(dbGeoArray.toArray(new DBGeoObject[dbGeoArray.size()]));
+				//the found informations are saved in a DBGeoObject, which is going to be saved in the DB
+				saveObject(dbGeoArray.toArray(new DBGeoObject[dbGeoArray.size()]));
 			}
 		}
 		catch(Exception e)
