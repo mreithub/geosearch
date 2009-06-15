e21ead817a7358dc15cf12cd1a2ace81212ab380
manuel_reithuber <manuel_reithuber@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-15 22:56:48 +0000

    website: log4j-init-servlet erzeugt (funzt noch nicht ganz...)
    git-svn-id: https://svn.r7r.at/sepm/trunk@341 240b89b8-79f3-4156-a269-5c34a98ffd57

---
+++ website/src/main/java/at/fakeroot/sepm/server/Log4jInit.java
@@ -0,0 +1,30 @@
+package at.fakeroot.sepm.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jInit extends HttpServlet {
+	public void init(ServletConfig config) throws ServletException {
+		Properties prop = new Properties();
+		String logfile = getInitParameter("log4j-init-file");
+		
+		InputStream propStream = getClass().getResourceAsStream(logfile);
+		if (propStream == null) {
+			throw new ServletException ("Error: Couldn't open property file '"+logfile+"'");
+		}
+		try {
+			prop.load(propStream);
+		}
+		catch (IOException e) {
+			throw new ServletException("Couldn't load log4j property file", e);
+		}
+
+		PropertyConfigurator.configure(prop);
+	}
+}
