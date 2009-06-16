3ee8554341b210249f168e2505defaa3b298f959
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-16 08:21:23 +0000

    LocrCrawler auf leere abragen getestet
    git-svn-id: https://svn.r7r.at/sepm/trunk@347 240b89b8-79f3-4156-a269-5c34a98ffd57

--- crawler.locr/src/main/java/at/fakeroot/sepm/crawler/locr/LocrCrawler.java
+++ crawler.locr/src/main/java/at/fakeroot/sepm/crawler/locr/LocrCrawler.java
@@ -34,7 +34,7 @@ public class LocrCrawler extends ACrawler {
 	
 	private void recCrawl(BoundingBox curBox, int start){
 		String url = "http://de.locr.com/api/get_photos_json.php"
-			+ "?latitudemin=" + curBox.getX1() + 
+			+ "?latitudemin=" + curBox.getX1()+ 
 			"&longitudemin=" + curBox.getY1() + 
 			"&latitudemax=" + curBox.getX2()
 			+ "&longitudemax=" + curBox.getY2()
@@ -48,52 +48,57 @@ public class LocrCrawler extends ACrawler {
 		//System.out.println(crawl(url));
 	
 		try {
-			ArrayList<String> tags;
-		    Property[] myProperty = new Property[3];
-		    
-			JSONObject myJSONObject = new JSONObject(requestUrl(url));
-			//System.out.println("Parse: "+myJSONObject.getJSONArray("photos"));
-			JSONArray myArray = myJSONObject.getJSONArray("photos");
-			
-			DBGeoObject[] saveDBArray = new DBGeoObject[myArray.length()];
-			System.out.println("arrayLength"+myArray.length());
-			for(int k=0;k<myArray.length();k++){
-				//pic url small: {picPath}{picName}_s.{picExt}
-				//pic url medium: {picPath}{picName}_m.{picExt}
-				myProperty[0]= new Property("picPath",(((JSONObject)myArray.get(k)).get("photo_path").toString()));
-			    myProperty[1]= new Property("picName",(((JSONObject)myArray.get(k)).get("photo_name").toString()));
-			    myProperty[2]= new Property("picExt",(((JSONObject)myArray.get(k)).get("photo_ext").toString()));
-				
+			String requestString=requestUrl(url);
+			if(requestString!=null){
+				ArrayList<String> tags;
+			    Property[] myProperty = new Property[3];
 			    
-			    //Tags
-			    tags = new ArrayList<String>();
-			    parseStringIntoTags((((JSONObject)myArray.get(k)).get("tags").toString()), tags, false);
-			    parseStringIntoTags((((JSONObject)myArray.get(k)).get("location_name").toString()), tags, false);
-			    
-				//Integer.parseInt((((JSONObject)myArray.get(k)).get("photo_id").toString()));
-				saveDBArray[k]=new DBGeoObject(
-						Integer.parseInt((((JSONObject)myArray.get(k)).get("photo_id").toString())),
-						(((JSONObject)myArray.get(k)).get("location_name").toString()),
-						Double.parseDouble((((JSONObject)myArray.get(k)).get("latitude").toString())),
-						Double.parseDouble((((JSONObject)myArray.get(k)).get("longitude").toString())),
-						getSvcID(),
-						(((JSONObject)myArray.get(k)).get("photo_id").toString()),
-						(((JSONObject)myArray.get(k)).get("photo_url").toString()),
-						null,
-						myProperty,
-						tags.toArray(new String[tags.size()]));
+				JSONObject myJSONObject = new JSONObject(requestString);
+				//System.out.println("Parse: "+myJSONObject.getJSONArray("photos"));
+				JSONArray myArray = myJSONObject.getJSONArray("photos");
 				
+				int picArrLength=myArray.length();
+				DBGeoObject[] saveDBArray = new DBGeoObject[picArrLength];
+				System.out.println("arrayLength"+myArray.length());
+				for(int k=0;k<myArray.length();k++){
+					//pic url small: {picPath}{picName}_s.{picExt}
+					//pic url medium: {picPath}{picName}_m.{picExt}
+					myProperty[0]= new Property("picPath",(((JSONObject)myArray.get(k)).get("photo_path").toString()));
+				    myProperty[1]= new Property("picName",(((JSONObject)myArray.get(k)).get("photo_name").toString()));
+				    myProperty[2]= new Property("picExt",(((JSONObject)myArray.get(k)).get("photo_ext").toString()));
+					
+				    
+				    //Tags
+				    tags = new ArrayList<String>();
+				    parseStringIntoTags((((JSONObject)myArray.get(k)).get("tags").toString()), tags, false);
+				    parseStringIntoTags((((JSONObject)myArray.get(k)).get("location_name").toString()), tags, false);
+				    
+					//Integer.parseInt((((JSONObject)myArray.get(k)).get("photo_id").toString()));
+					saveDBArray[k]=new DBGeoObject(
+							Integer.parseInt((((JSONObject)myArray.get(k)).get("photo_id").toString())),
+							(((JSONObject)myArray.get(k)).get("location_name").toString()),
+							Double.parseDouble((((JSONObject)myArray.get(k)).get("latitude").toString())),
+							Double.parseDouble((((JSONObject)myArray.get(k)).get("longitude").toString())),
+							getSvcID(),
+							(((JSONObject)myArray.get(k)).get("photo_id").toString()),
+							(((JSONObject)myArray.get(k)).get("photo_url").toString()),
+							null,
+							myProperty,
+							tags.toArray(new String[tags.size()]));
+					
+				}
+				
+				if(picArrLength>0){
+					saveObject(saveDBArray);
+				}
+				
+				if(myArray.length()==stepSize){
+					System.out.println("rec");
+					recCrawl(curBox, start+stepSize);
+				}else{
+					System.out.println("noRec");
+				}
 			}
-			
-			saveObject(saveDBArray);
-			
-			if(myArray.length()==stepSize){
-				System.out.println("rec");
-				recCrawl(curBox, start+stepSize);
-			}else{
-				System.out.println("noRec");
-			}
-			
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
