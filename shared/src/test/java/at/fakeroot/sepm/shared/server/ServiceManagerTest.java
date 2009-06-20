7a21aac0b94fdcf852f28600cd9df210dc76f79c
anca_cismasiu <anca_cismasiu@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-20 17:56:35 +0000

    serviceManagertest geandert
    git-svn-id: https://svn.r7r.at/sepm/trunk@359 240b89b8-79f3-4156-a269-5c34a98ffd57

--- shared/src/test/java/at/fakeroot/sepm/shared/server/ServiceManagerTest.java
+++ shared/src/test/java/at/fakeroot/sepm/shared/server/ServiceManagerTest.java
@@ -9,9 +9,25 @@ public class ServiceManagerTest extends TestCase{
 	ServiceManager servManager;
 
 	public void testSimple(){
-		
+		int wikiId=-1;
+	
 		try{
-			DBService dbServ = servManager.select(1);
+			DBService dbSvc = servManager.select("de.wikipedia.org");
+			wikiId=dbSvc.getSvc_id();
+	//		assertEquals(1, dbSvc.getSvc_id());
+			assertEquals("de.wikipedia.org", dbSvc.getName());
+			assertEquals("Wikipedia", dbSvc.getTitle());
+			assertEquals("http://de.wikipedia.org/", dbSvc.getHomepage());
+			assertEquals("Deutsche Wikipedia (freies Online-Lexicon)", dbSvc.getDescription());
+			assertEquals(3, dbSvc.getSType_id());
+			assertEquals("<div>%description%</div>", dbSvc.getBubbleHTML());
+			
+		}catch(Exception e){
+			fail("could not retrieve service by name:"+ e.getMessage());
+			
+		}
+			try{
+			DBService dbServ = servManager.select(wikiId);
 			assertEquals("de.wikipedia.org", dbServ.getName());
 			assertEquals("Wikipedia", dbServ.getTitle());
 			assertEquals("http://de.wikipedia.org/", dbServ.getHomepage());
@@ -24,20 +40,6 @@ public class ServiceManagerTest extends TestCase{
 			fail("could not retrieve service by id:"+ e.getMessage());
 		}
 		
-		try{
-			DBService dbSvc = servManager.select("de.wikipedia.org");
-			assertEquals(1, dbSvc.getSvc_id());
-			assertEquals("de.wikipedia.org", dbSvc.getName());
-			assertEquals("Wikipedia", dbSvc.getTitle());
-			assertEquals("http://de.wikipedia.org/", dbSvc.getHomepage());
-			assertEquals("Deutsche Wikipedia (freies Online-Lexicon)", dbSvc.getDescription());
-			assertEquals(3, dbSvc.getSType_id());
-			assertEquals("<div>%description%</div>", dbSvc.getBubbleHTML());
-			
-		}catch(Exception e){
-			fail("could not retrieve service by name:"+ e.getMessage());
-			
-		}
 	}
 
 	@Override
