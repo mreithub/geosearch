ae5134bc5f5a4aa37599a8d150a4875ed09a2f9f
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-06-24 08:00:06 +0000

    IGeoSave:JavaDoc
    git-svn-id: https://svn.r7r.at/sepm/trunk@406 240b89b8-79f3-4156-a269-5c34a98ffd57

--- shared/src/main/java/at/fakeroot/sepm/shared/server/IGeoSave.java
+++ shared/src/main/java/at/fakeroot/sepm/shared/server/IGeoSave.java
@@ -6,9 +6,21 @@ import at.fakeroot.sepm.shared.server.DBGeoObject;
 
 
 public interface IGeoSave extends Remote {
+	/**
+	 * Insert or update a GeoObject in the DB
+	 * @param newGeoObj 
+	 */
 	public void saveObject(DBGeoObject geoObj)throws RemoteException;
 	
+	/**
+	 * Insert or update a array of GeoObjects in the DB
+	 * @param objects 
+	 */
 	public void saveObjects(DBGeoObject objects[]) throws RemoteException;
 	
+	/**
+	 * Return the serviceID of an given serviceName
+	 * @param svcName serviceName (eg. panoramio.com)
+	 */
 	public int getServiceID(String svcName)throws RemoteException;
 }
