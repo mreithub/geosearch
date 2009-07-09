8ab5bf73e832c04d27d535c30dbae20ac97e3ca7
mathias_kunter <mathias_kunter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-07-09 19:29:58 +0000

    JUnit Test mit MOCK-Objekten für IGeoSaveImpl erstellt
    git-svn-id: https://svn.r7r.at/sepm/trunk@532 240b89b8-79f3-4156-a269-5c34a98ffd57

--- webapp/src/test/java/at/fakeroot/sepm/server/IGeoSaveImplTest.java
+++ webapp/src/test/java/at/fakeroot/sepm/server/IGeoSaveImplTest.java
@@ -19,18 +19,45 @@ public class IGeoSaveImplTest
 	
 	@Test
 	/**
-	 * This function tests if the insertion of a new object works.
+	 * This function tests if the insertion of a new object works. Uses Mock objects.
 	 */
 	public void testInsert() throws Exception
 	{
+		//Create the Mock object.
 		mockManager = createStrictMock(IGeoObjectManager.class);
 		IGeoSaveImpl geoSave = new IGeoSaveImpl(mockManager);
 		
+		//Create the geo object to test with.
 		DBGeoObject geoObj = new DBGeoObject(3, "abc", 1.11, 2.22, 17, "uid", "link", null, null, null);
 		
+		//Define the expected function calls to the Mock object.
 		expect(mockManager.getObjectId(eq(17), eq("uid"))).andThrow(new NotFoundException());
 		mockManager.insert(geoObj);
 		
+		//Test if the expected function calls are actually made when the test runs.
+		replay(mockManager);
+		geoSave.saveObject(geoObj);
+		verify(mockManager);
+	}
+	
+	@Test
+	/**
+	 * This function tests if the update of an already existing object works. Uses Mock objects.
+	 */
+	public void testUpdate() throws Exception
+	{
+		//Create the Mock object.
+		mockManager = createStrictMock(IGeoObjectManager.class);
+		IGeoSaveImpl geoSave = new IGeoSaveImpl(mockManager);
+		
+		//Create the geo object to test with.
+		DBGeoObject geoObj = new DBGeoObject(11, "abc", 1.11, 2.22, 17, "uid", "link", null, null, null);
+		
+		//Define the expected function calls to the Mock object.
+		expect(mockManager.getObjectId(eq(17), eq("uid"))).andReturn((long)11);
+		mockManager.update(geoObj);
+		
+		//Test if the expected function calls are actually made when the test runs.
 		replay(mockManager);
 		geoSave.saveObject(geoObj);
 		verify(mockManager);
