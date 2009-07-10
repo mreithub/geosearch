3c2ceee3e32704ce734046634d403c0893b3319e
manuel_reithuber <manuel_reithuber@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-07-10 17:58:48 +0000

    DBConnectionTest überarbeitet
    git-svn-id: https://svn.r7r.at/sepm/trunk@559 240b89b8-79f3-4156-a269-5c34a98ffd57

--- webapp/src/test/java/at/fakeroot/sepm/server/DBConnectionTest.java
+++ webapp/src/test/java/at/fakeroot/sepm/server/DBConnectionTest.java
@@ -21,121 +21,77 @@ import org.junit.Test;
 public class DBConnectionTest {
 	IDBConnection db = null;
 
-	
-	
 	@Test
 	public void testWriteUpdateRead() throws SQLException {
-		// only do this if we are on the testing database
-		if (db.isTesting()) {
-			//Insert
-			String testName="testWriteRead2";
-			String testImg="/img/testWriteRead2.png";
-			
-			Statement s = db.createStatement();
-			
-			s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testWriteRead', '/img/testWriteRead1.png')");
-			s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('"+testName+"', '"+testImg+"')");
+		//Insert
+		String testName="testWriteRead2";
+		String testImg="/img/testWriteRead2.png";
+		
+		Statement s = db.createStatement();
+		
+		s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testWriteRead', '/img/testWriteRead1.png')");
+		s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('"+testName+"', '"+testImg+"')");
 
-			ResultSet r = s.executeQuery("SELECT * FROM serviceType WHERE name='"+testName+"'");
-			
-			r.next();
-			String testNameBack=r.getString("name");
-			String testImgBack=r.getString("thumbnail");
+		ResultSet r = s.executeQuery("SELECT * FROM serviceType WHERE name='"+testName+"'");
+		
+		r.next();
+		assertEquals(testName, r.getString("name"));
+		assertEquals(testImg, r.getString("thumbnail"));
 
-			
-			//Update
-			String testTestImg="/img/testWriteRead3.png";
-			
+		
+		//Update
+		String testTestImg="/img/testWriteRead3.png";
+		
 
-			s.executeUpdate("UPDATE serviceType SET thumbnail='"+testTestImg+"' WHERE name='"+testName+"'");
-			r = s.executeQuery("SELECT * FROM serviceType WHERE name='"+testName+"'");
-			
-			r.next();
-			String testTestNameBack=r.getString("name");
-			String testTestImgBack=r.getString("thumbnail");
-			
-			
-			// make sure that the DB transaction is rolled back before we run an assert 
-			db.disconnect();
-			
-			
-			assertEquals(testNameBack, testName);
-			assertEquals(testImgBack, testImg);
-			
-			assertEquals(testNameBack, testTestNameBack);
-			assertEquals(testTestImg, testTestImgBack);
-		}else{
-			fail("Not using the testing database");
-		}
+		s.executeUpdate("UPDATE serviceType SET thumbnail='"+testTestImg+"' WHERE name='"+testName+"'");
+		r = s.executeQuery("SELECT * FROM serviceType WHERE name='"+testName+"'");
+		
+		r.next();
+		assertEquals(testName, r.getString("name"));
+		assertEquals(testTestImg, r.getString("thumbnail"));
 	}
 	
 	@Test
 	public void testWriteRead() throws SQLException {
-		// only do this if we are on the testing database
-		if (db.isTesting()) {
-			String testName="testWriteRead2";
-			String testImg="/img/testWriteRead2.png";
-			
-			Statement s = db.createStatement();
-			s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testWriteRead', '/img/testWriteRead1.png')");
-			s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('"+testName+"', '"+testImg+"')");
+		String testName="testWriteRead2";
+		String testImg="/img/testWriteRead2.png";
+		
+		Statement s = db.createStatement();
+		s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testWriteRead', '/img/testWriteRead1.png')");
+		s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('"+testName+"', '"+testImg+"')");
 
-			ResultSet r = s.executeQuery("SELECT * FROM serviceType WHERE name='testWriteRead2'");
-			
-			/*
-			while(r.next()){
-				System.out.println("rowCount: "+r.getRow());
-				System.out.println("n:"+r.getString("name"));
-				System.out.println("i:"+r.getString("thumbnail"));
-			}
-			*/
-			r.next();
-			String testNameBack=r.getString("name");
-			String testImgBack=r.getString("thumbnail");
+		ResultSet r = s.executeQuery("SELECT * FROM serviceType WHERE name='testWriteRead2'");
 
-			// make sure that the DB transaction is rolled back before we run an assert 
-			db.disconnect();
-			
-			
-			assertEquals(testNameBack, testName);
-			assertEquals(testImgBack, testImg);
-		}else{
-			fail("Not using the testing database!");
-		}
+		r.next();
+		assertEquals(testName, r.getString("name"));
+		assertEquals(testImg, r.getString("thumbnail"));
 	}
 	
 	@Test
 	public void testWrite() throws SQLException {
-		// only do this if we are on the testing database
-		if (db.isTesting()) {
-			Statement s = db.createStatement();
-			s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testtype', '/img/test1.png')");
-			s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testtype2', '/img/test2.png')");
+		Statement s = db.createStatement();
+		s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testtype', '/img/test1.png')");
+		s.executeUpdate("INSERT INTO serviceType (name, thumbnail) VALUES ('testtype2', '/img/test2.png')");
 
-			ResultSet r = s.executeQuery("SELECT name FROM serviceType");
-			
+		ResultSet r = s.executeQuery("SELECT name FROM serviceType");
 
-			// check if rowcount = 5
-			r.last();
-			int rowcount = r.getRow();
-
-			// make sure that the DB transaction is rolled back before we run an assert 
-			db.disconnect();
-		
-			assertEquals(3, rowcount);
- 		}else{
-			fail("Not using the testing database!");
-		}
+		// check for the correct number of rows
+		// (one permanent test serviceType and the two we've just created) 
+		r.last();
+	
+		assertEquals(3, r.getRow());
 	}
 
 	@Before
 	public void setUp() throws FileNotFoundException, IOException {
 		db = new DBConnection();
-		
+		// only test if we are on the testing database
+		// (which should always be true because of a specific jdbc.properties file in src/test/resources)
+		assertTrue("Not using the  testing database!", db.isTesting());
 	}
 	
 	@After
-	public void tearDown() {
-		
+	public void tearDown() throws SQLException {
+		db.disconnect();
 	}
 }
