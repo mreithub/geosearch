e69cc8124a1af4503e8c27d1e919000508373660
manuel_reithuber <manuel_reithuber@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-07-10 16:27:01 +0000

    ServiceManagerTest überarbeitet
    git-svn-id: https://svn.r7r.at/sepm/trunk@553 240b89b8-79f3-4156-a269-5c34a98ffd57

--- webapp/src/test/java/at/fakeroot/sepm/server/ServiceManagerTest.java
+++ webapp/src/test/java/at/fakeroot/sepm/server/ServiceManagerTest.java
@@ -3,6 +3,10 @@ package at.fakeroot.sepm.server;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,64 +17,64 @@ import org.junit.Test;
  */
 public class ServiceManagerTest
 {
-	private ServiceManager svcManger;
+	private ServiceManager svcManager;
 	private DBService dbService;
 	private DBService result;
 	
 	@Before
-	public void setUp()
+	public void setUp() throws SQLException, IOException
 	{
-		svcManger = new ServiceManager();
-		dbService = new DBService(15, "example.com", "TestService", "http://www.example.com/", 
+		svcManager = new ServiceManager();
+		String svcName = "example.com";
+		// the service id is generated automatically by the database => first request it
+		int svcId = svcManager.select(svcName).getSvc_id();
+		// initialize a DBService objects with the data of the test service in the database 
+		dbService = new DBService(svcId, svcName, "TestService", "http://www.example.com/", 
 				"Test-Service", 12, "%description%", "images/service/wikipedia.png");
 		dbService.setTags(new String[] {"stag1"});
 	}
 	
+	/**
+	 * Select by service ID
+	 */
 	@Test
-	public void test_selectSvcId() 
+	public void test_selectBySvcId() throws FileNotFoundException, IOException, SQLException 
 	{
-		try
-		{
-			if(DBConnection.staticIsTesting())
-			{	
-				result = svcManger.select(dbService.getSvc_id());
-				assertNotNull(result);
-				assertEquals(dbService.getName(), result.getName());
-				assertEquals(dbService.getTitle(), result.getTitle());
-				assertEquals(dbService.getHomepage(), result.getHomepage());
-				assertEquals(dbService.getDescription(), result.getDescription());
-				assertEquals(dbService.getSType_id(), result.getSType_id());
-				assertEquals(dbService.getBubbleHTML(), result.getBubbleHTML());
-				assertEquals(dbService.getThumbnail(), result.getThumbnail());
-				assertEquals(dbService.getTags()[0], result.getTags()[0]);
-				assertEquals(dbService.getTags().length, result.getTags().length);
-			}
+		if(DBConnection.staticIsTesting())
+		{	
+			result = svcManager.select(dbService.getSvc_id());
+			assertNotNull(result);
+			assertEquals(dbService.getName(), result.getName());
+			assertEquals(dbService.getTitle(), result.getTitle());
+			assertEquals(dbService.getHomepage(), result.getHomepage());
+			assertEquals(dbService.getDescription(), result.getDescription());
+			assertEquals(dbService.getSType_id(), result.getSType_id());
+			assertEquals(dbService.getBubbleHTML(), result.getBubbleHTML());
+			assertEquals(dbService.getThumbnail(), result.getThumbnail());
+			assertEquals(dbService.getTags()[0], result.getTags()[0]);
+			assertEquals(dbService.getTags().length, result.getTags().length);
 		}
-		catch(Exception e)
-		{}
 	}
 	
+	/**
+	 * select by service name
+	 */
 	@Test
-	public void test_selectSvcName() 
+	public void test_selectBySvcName() throws FileNotFoundException, IOException, SQLException 
 	{
-		try
-		{
-			if(DBConnection.staticIsTesting())
-			{	
-				result = svcManger.select(dbService.getName());
-				assertNotNull(result);
-				assertEquals(dbService.getSvc_id(), result.getSvc_id());
-				assertEquals(dbService.getTitle(), result.getTitle());
-				assertEquals(dbService.getHomepage(), result.getHomepage());
-				assertEquals(dbService.getDescription(), result.getDescription());
-				assertEquals(dbService.getSType_id(), result.getSType_id());
-				assertEquals(dbService.getBubbleHTML(), result.getBubbleHTML());
-				assertEquals(dbService.getThumbnail(), result.getThumbnail());
-				assertEquals(dbService.getTags()[0], result.getTags()[0]);
-				assertEquals(dbService.getTags().length, result.getTags().length);
-			}
+		if(DBConnection.staticIsTesting())
+		{	
+			result = svcManager.select(dbService.getName());
+			assertNotNull(result);
+			assertEquals(dbService.getSvc_id(), result.getSvc_id());
+			assertEquals(dbService.getTitle(), result.getTitle());
+			assertEquals(dbService.getHomepage(), result.getHomepage());
+			assertEquals(dbService.getDescription(), result.getDescription());
+			assertEquals(dbService.getSType_id(), result.getSType_id());
+			assertEquals(dbService.getBubbleHTML(), result.getBubbleHTML());
+			assertEquals(dbService.getThumbnail(), result.getThumbnail());
+			assertEquals(dbService.getTags()[0], result.getTags()[0]);
+			assertEquals(dbService.getTags().length, result.getTags().length);
 		}
-		catch(Exception e)
-		{}
 	}
 }
