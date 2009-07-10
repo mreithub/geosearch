160dc19ca95fceb48aecefe6d811f36366088ab0
juergen_brandstetter <juergen_brandstetter@240b89b8-79f3-4156-a269-5c34a98ffd57>
2009-07-10 16:41:12 +0000

    DBSearviceTest
    git-svn-id: https://svn.r7r.at/sepm/trunk@554 240b89b8-79f3-4156-a269-5c34a98ffd57

--- webapp/src/test/java/at/fakeroot/sepm/server/DBServiceTest.java
+++ webapp/src/test/java/at/fakeroot/sepm/server/DBServiceTest.java
@@ -1,6 +1,8 @@
 package at.fakeroot.sepm.server;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -10,21 +12,21 @@ import static org.junit.Assert.*;
  * */
 
 public class DBServiceTest {
-
+	
+	private DBService serv;
+	private int svc_id=10;
+	private String name="myService.com";
+	private String title="myService";
+	private String homepage = "www.myService.com";
+	private String description = "Describing myService";
+	private int sTypeId = 1;
+	private String bubbleHTML = "bubble";
+	private String thumbnail = "www.myServiceThumbnail.com";
+	
+	
 	@Test
-	public void testSimple(){
-		
-		int svc_id=10;
-		String name="myService.com";
-		String title="myService";
-		String homepage = "www.myService.com";
-		String description = "Describing myService";
-		int sTypeId = 1;
-		String bubbleHTML = "bubble";
-		String thumbnail = "www.myServiceThumbnail.com";
-		
-		DBService serv = new DBService(svc_id, name, title, homepage, description, sTypeId, bubbleHTML, thumbnail);
-		
+	public void testGetter(){
+
 		assertEquals(svc_id, serv.getSvc_id());
 		assertEquals(name, serv.getName());
 		assertEquals(title, serv.getTitle());
@@ -32,6 +34,30 @@ public class DBServiceTest {
 		assertEquals(description, serv.getDescription());
 		assertEquals(sTypeId, serv.getSType_id());
 		assertEquals(bubbleHTML, serv.getBubbleHTML());
+		assertEquals(thumbnail, serv.getThumbnail());
 	}
+	
+	@Test
+	public void testTags(){
+		String[] tags = new String[]{"tag1","tag2"};
+		
+		assertEquals(serv.getTags().length, 0);
+		
+		serv.setTags(tags);
+		assertEquals(serv.getTags()[0], "tag1");
+		assertEquals(serv.getTags()[1], "tag2");
+	}
+	
+	
+	@After
+	public void tearDown() throws Exception {
+
+	}
+	
+	@Before
+	public void setUp(){
+		serv = new DBService(svc_id, name, title, homepage, description, sTypeId, bubbleHTML, thumbnail);
+	}
+	
 }
 
